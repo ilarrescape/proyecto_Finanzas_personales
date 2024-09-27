a7bacaefd682094b5b654e5636ca9229c8f489fb
Mauricio Ilarrescape <ilarrescapem@gmail.com>
2024-09-26 21:48:32 -0300

    primer commit clase 34

--- app_finanzas.py
+++ app_finanzas.py
@@ -1,6 +1,7 @@
 import streamlit as st 
 import pandas as pd
 
+from moneda import fun_moneda
 
 def mostrar_home():
     st.title('Bienvenido a la Aplicación de Finanzas Personales :smile:')
@@ -9,17 +10,37 @@ def main():
     if 'page' not in st.session_state:
         st.session_state.page = 'Inicio'
 
+    st.markdown(
+    """
+        <style>
+            .st-emotion-cache-igwvy71 h1{
+                color: white;
+                text-align: center;
+            }
+            .st-emotion-cache-8atqhb {
+                padding-left: 1em;
+                padding-right: 1em;
+                padding-bottom: 1.5em;
+                border-radius: 0.5em;
+                border: 1px solid rgba(0, 0, 0, 0.5);
+                background-color: black;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+    
     st.sidebar.title('Menú de Opciones')
 
-    if st.sidebar.button('Inicio'):
+    if st.sidebar.button('Inicio', use_container_width=True):
         st.session_state.page = 'Inicio'
-    if st.sidebar.button('Registro de Usuario'):
+    if st.sidebar.button('Registro de Usuario', use_container_width= True):
         st.session_state.page = 'registro-usuario'
-    if st.sidebar.button('Configurar Moneda'):
+    if st.sidebar.button('Configurar Moneda',use_container_width=True):
         st.session_state.page = 'config-currency'
         
     if st.session_state.page == 'Inicio':
         mostrar_home()
+    if st.session_state.page == 'config-currency':
+        fun_moneda()
     # if st.session_state.page == 'registro-usuario':
     #     #mostrar_registro()
 
--- moneda.py
+++ moneda.py
@@ -3,6 +3,7 @@ import streamlit as st
 from dotenv import load_dotenv
 import pandas as pd
 import os
+import time
 
 load_dotenv()
 
@@ -22,12 +23,22 @@ class Moneda:
         return result
     
     def agregar_datos(self,nombre):
-        self.cursor.execute('SELECT * FROM moneda ORDER BY id_moneda DESC LIMIT 1')
-        list_diccionario = self.cursor.fetchall()
-        nuevo_id = list_diccionario[0]['id_moneda']
-        nuevo_id += 1
-        self.cursor.execute('INSERT INTO moneda (id_moneda, nombre_moneda) VALUES (%s, %s)',(nuevo_id, nombre))
-        self.connection.commit()
+        #Solución planteada al problema expuesto por los alumnos:
+        #--->Qué pasa si no tenemos registros en la tabla moneda
+        try:
+            self.cursor.execute('SELECT * FROM moneda ORDER BY id_moneda DESC LIMIT 1')
+            list_diccionario = self.cursor.fetchall()
+            nuevo_id = list_diccionario[0]['id_moneda']
+            nuevo_id += 1
+            self.cursor.execute('INSERT INTO moneda (id_moneda, nombre_moneda) VALUES (%s, %s)',(nuevo_id, nombre))
+            self.connection.commit()
+        except:
+            st.info('La tabla está vacía. El siguiente registro será el primero. :smile:')
+            time.sleep(2)
+            nuevo_id = 1
+            self.cursor.execute('INSERT INTO moneda (id_moneda, nombre_moneda) VALUES (%s, %s)',(nuevo_id, nombre))
+            self.connection.commit()
+            st.rerun()
     
     def actualizar_datos(self, id, nombre):
         self.cursor.execute('UPDATE moneda SET nombre_moneda = %s WHERE id_moneda = %s',(nombre, id))
@@ -40,15 +51,50 @@ class Moneda:
 class DataManagerMoneda:
     def __init__(self) -> None:
         self.db_moneda = Moneda()
+        
+    @st.dialog('Modificar Moneda')
+    def modify_moneda(self, id, nombre):
+        col_a, col_b, col_c = st.columns([0.5,9,0.5])
+        with col_b:
+            nombre_moneda = st.text_input('Ingrese la moneda: ', value=nombre)
+            col_I, col_II, col_III = st.columns([5,4,4])
+            with col_II:
+                if st.button('Modificar',use_container_width= True):
+                    self.db_moneda.actualizar_datos(id, nombre_moneda)
+                    st.rerun()
+            with col_III:
+                if st.button('Eliminar', use_container_width= True):
+                    self.db_moneda.eliminar_datos(id)
+                    st.rerun()
     
     def display_moneda(self):
         data_moneda = self.db_moneda.obtener_datos()
         dataframe_moneda = pd.DataFrame(data_moneda)
         dataframe_moneda = dataframe_moneda.rename(columns={'id_moneda':'ID', 'nombre_moneda':'NOMBRE'})
-        st.dataframe(dataframe_moneda, height=175, use_container_width=True, hide_index=True)
-
-objeto_divisa = Moneda()
-
-
-
-st.write(objeto_divisa.obtener_datos())
+        evento = st.dataframe(dataframe_moneda,
+                    height=175,
+                    use_container_width=True,
+                    hide_index=True, 
+                    selection_mode="single-row",
+                    on_select="rerun")
+        if evento['selection']['rows']:
+            filtrado = dataframe_moneda[dataframe_moneda.index.isin(evento.selection['rows'])]
+            valor_id = int(filtrado.iloc[0,0])
+            valor_nombre = str(filtrado.iloc[0,1])
+            self.modify_moneda(valor_id, valor_nombre)
+    
+    def add_moneda(self):
+        cont_add_moneda = st.container(border=True)
+        with cont_add_moneda:
+            st.subheader('Agregar Nueva Moneda')
+            nombre_moneda = st.text_input('Ingresar Moneda: ')
+            if st.button('Guardar Moneda'):
+                self.db_moneda.agregar_datos(nombre_moneda)
+    
+def fun_moneda():
+    objeto_moneda = DataManagerMoneda()
+    col_I, col_II = st.columns(2)
+    with col_I:
+        objeto_moneda.add_moneda()
+    with col_II:
+        objeto_moneda.display_moneda()
